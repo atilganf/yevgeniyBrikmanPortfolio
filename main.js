b0a5222524e926e75c40005c4c3ec8c521ddef81
atilganf <atilganyf@gmail.com>
2019-08-28 21:33:24 +0300

    save commit

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-$(document).ready(function(){
-  var x = 159;
-  $(".toggleButton").click(function(){
-    $(".collapsible").slideToggle("fast","linear");
-    var px = "+=" + x.toString() + "px";
-    $("#header").animate({top: px}, "fast");
-    $("#body").animate({top: px}, "fast");
-    $("#footer").animate({top: px}, "fast");
-    x = -x;
-    var px = "+=" + x.toString() + "px";
-    $(".res").animate({top: px}, "fast");
-  });
+$(document).ready(function () {
+   $(".toggleButton").click(function(){
+      $(".collapsible").slideToggle(300, "swing");
+   });
 });
+
